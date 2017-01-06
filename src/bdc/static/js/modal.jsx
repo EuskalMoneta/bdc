f30fe2bcf8990aceae3526bc2d01963db3c9ff70
Florian Le Frioux <florian.lefrioux@meta-it.fr>
2017-01-06 16:25:42 +0100

    [IMP] #729 - Ajout d'un écran de confirmation sur la page d'ajout d'un adhérent (à tester en profondeur surtout la sortie de la modale quand on annule)

--- src/bdc/static/js/modal.jsx
+++ src/bdc/static/js/modal.jsx
@@ -7,44 +7,63 @@ import {
     ModalFooter
 } from 'react-modal-bootstrap'
 
-class ModalEusko extends React.Component {
+export default class ModalEusko extends React.Component {
     constructor(props) {
         super(props);
 
         // Compute body (from props with labels and user data)
         var modalBody = this.props.modalBody
 
-        this.state = {
-            isModalOpen: false,
-            modalBody: modalBody,
-            cancelLabel: this.props.cancelLabel,
-            validateLabel: this.props.validateLabel
-        }
+        this.state = this.props
     }
 
-    openModal() {
-        this.setState({isModalOpen: true})
+    computeModalBody(modalBody) {
+        return _.map(modalBody, (item) => {
+            return (<div className="form-group row" key={item.label}>
+                        <label className="col-sm-3">{item.label} :</label>
+                        <div className="col-sm-6">
+                            <span>{item.value}</span>
+                        </div>
+                    </div>)
+        })
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps) {
+            // We need to do this because nextProps is read-only...
+            var newProps = _.mapObject(nextProps, (item) => { return item })
+
+            // Compute modalBody from new prop
+            newProps.modalBody = this.computeModalBody(newProps.modalBody)
+
+            // Display modalBody
+            this.setState(newProps)
+        }
     }
 
     hideModal() {
         this.setState({isModalOpen: false})
     }
 
+    onValidate() {
+        this.setState({isModalOpen: false}, this.props.onValidate)
+    }
+
     render() {
         return (
-            <Modal isOpen={this.state.isModalOpen} onRequestHide={this.hideModal}>
+            <Modal isOpen={this.state.isModalOpen} onRequestHide={this.hideModal.bind(this)}>
                 <ModalHeader>
-                    <ModalClose onClick={this.hideModal}/>
+                    <ModalClose onClick={this.hideModal.bind(this)}/>
                     <ModalTitle>{this.props.modalTitle}</ModalTitle>
                 </ModalHeader>
                 <ModalBody>
-                    <p>{this.state.modalBody}</p>
+                    <div>{this.state.modalBody}</div>
                 </ModalBody>
                 <ModalFooter>
-                    <button className="btn btn-default" onClick={this.hideModal}>
+                    <button className="btn btn-default" onClick={this.hideModal.bind(this)}>
                       {this.props.cancelLabel}
                     </button>
-                    <button onClick={this.handleDeleteBDC} className="btn btn-danger">
+                    <button onClick={this.onValidate.bind(this)} className="btn btn-success">
                       {this.props.validateLabel}
                     </button>
                 </ModalFooter>
@@ -56,13 +75,12 @@ class ModalEusko extends React.Component {
 // Specifies the default values and type checking for props
 ModalEusko.defaultProps = {
     cancelLabel: __("Annuler"),
-    validateLabel: __("Valider")
+    validateLabel: __("Valider"),
+    modalTitle: __("Confirmation")
 }
 
 ModalEusko.propTypes = {
-    modalBody: React.PropTypes.element.isRequired,
-    modalTitle: React.PropTypes.element.isRequired
+    onValidate: React.PropTypes.func.isRequired,
+    isModalOpen: React.PropTypes.bool.isRequired,
+    modalBody: React.PropTypes.array
 }
-
-
-export default ModalEusko;
