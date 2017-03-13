d0ea5ef627c253bebe3f3b8a38fa0f4d226c2e41
Florian Le Frioux <florian.lefrioux@meta-it.fr>
2017-03-13 10:53:23 +0100

    [IMP] #813 - Meilleure gestion de l'invalidité du token Cyclos côté front + Session expired message in login page

--- src/bdc/static/js/utils.jsx
+++ src/bdc/static/js/utils.jsx
@@ -1,21 +1,63 @@
 var checkStatus = (response) => {
-    if (response.status != 204 && response.status >= 200 && response.status < 300) {
+    if (response.status >= 200 && response.status <= 401) {
         return response
     }
-    else if (response.status == 204) {
-        var error = new Error("No content")
-        error.response = response
-        throw error
-    }
     else {
-        var error = new Error(response.statusText)
-        error.response = response
-        throw error
+        try {
+            // If we got a forbidden (403) response,
+            // *AND* we're not already in the login page
+            // *AND* the call we've made was for the Euskal Moneta API and *NOT* the Django front
+            // we redirect to the login page, in this page a "session expired" message will be displayed
+            if (response.statusText == "Forbidden"
+                && window.location.pathname.indexOf("/login") === -1
+                && response.url.indexOf(window.config.getAPIBaseURL) != -1) {
+                window.location.assign("/logout?next=" + window.location.pathname)
+            }
+            else {
+                var error = new Error(response.statusText)
+                error.response = response
+                throw error
+            }
+        }
+        catch(e) {
+            var error = new Error(response.statusText)
+            error.response = response
+            throw error
+        }
     }
 }
 
 var parseJSON = (response) => {
-    return response.json()
+    if (response.status == 204) {
+        return {}
+    }
+    else if (response.status == 400 || response.status == 401) {
+        var error = new Error(response.statusText)
+        error.response = response
+        throw error
+    }
+    else {
+        return response.json()
+    }
+}
+
+var checkSession = (data) => {
+    try {
+        if (data.detail.indexOf("LOGGED_OUT") != -1) {
+            window.location.assign("/logout?next=" + window.location.pathname)
+        }
+        else if (data.detail.indexOf("Exception") != -1) {
+            var error = new Error(data)
+            error.response = data
+            throw error
+        }
+        else {
+            return data
+        }
+    }
+    catch(e) {
+        return data
+    }
 }
 
 var storeToken = (data) => {
@@ -55,6 +97,7 @@ var fetchCustom = (url, method, promise, token, data, promiseError=null) => {
     fetch(url, payload)
     .then(checkStatus)
     .then(parseJSON)
+    .then(checkSession)
     .then(promise)
     .catch(promiseError)
 }
@@ -102,7 +145,8 @@ var getUrlParameter = (name) => {
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
 
-var isMemberIdEusko = (values, value) => {
+var isMemberIdEusko = (values, value) =>
+{
     if (!value) {
         return false
     }
@@ -217,10 +261,12 @@ var NavbarRight = React.createClass({
 
     componentDidMount() {
         // Get bdc name
-        var computeData = (data) => {
-            this.setState({bdcName: data})
+        if (window.config.userAuth) {
+            var computeData = (data) => {
+                this.setState({bdcName: data})
+            }
+            fetchAuth(getAPIBaseURL + "bdc-name/", 'get', computeData)
         }
-        fetchAuth(getAPIBaseURL + "bdc-name/", 'get', computeData)
     },
 
     toggleDropdown() {
@@ -317,6 +363,7 @@ class SelectizeUtils {
 module.exports = {
     checkStatus: checkStatus,
     parseJSON: parseJSON,
+    checkSession: checkSession,
     fetchAuth: fetchAuth,
     fetchCustom: fetchCustom,
     fetchGetToken: fetchGetToken,
